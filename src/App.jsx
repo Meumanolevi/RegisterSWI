94def0679ef841a031da058cd36520c1f14a0542
Meumanolevi <victorleviramos9@gmail.com>
2025-11-28 18:47:29 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+// Importe suas páginas (que ainda vamos criar)
+// import Home from './pages/Home'
+// import ClientRegister from './pages/ClientRegister'
+// import ClientConsult from './pages/ClientConsult'
+// ...outras páginas
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+      {/* <Route path="/clientes/cadastro" element={<ClientRegister />} /> */}
+      {/* <Route path="/clientes/consulta" element={<ClientConsult />} /> */}
+      {/* Adicione as outras rotas aqui */}
+    </Routes>
+  )
+}
+
+export default App
